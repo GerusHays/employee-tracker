5fa3211ef6404d43481f4b0f31c3bb35e2f774ab
Hays4Days <gerushays@gmail.com>
2022-06-23 17:53:48 -0700

    seeds and table created

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS library;
-CREATE DATABASE library;
-USE library;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
 
 CREATE TABLE department (
   id INT AUTO_INCREMENT PRIMARY KEY,
---
+++ db/seed.sql
@@ -0,0 +1,23 @@
+USE DATABASE employee_db;
+
+INSERT INTO department (name)
+VALUES 
+('Electronics'),
+('Produce'),
+('Automotive'),
+('Pharmacy');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Cashier', 25000, 1),
+('Pharmacist', 85000, 2),
+('Mechanic', 65000, 3),
+('Manager', 200000, 4);
+
+INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+VALUES 
+('Ronald', 'Firbank', 1, 5),
+  ('Virginia', 'Woolf', 1, 5),
+  ('Piers', 'Gaveston', 2, 5),
+  ('Charles', 'LeRoi', 3, 5),
+  ('Katherine', 'Mansfield', 4, NULL);
