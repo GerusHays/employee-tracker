636444eaf5f1755f6933a117fc84f2e258f4f195
Hays4Days <gerushays@gmail.com>
2022-06-23 19:49:16 -0700

    queries and department done

---
+++ queries/departmentQuery.js
@@ -0,0 +1,81 @@
+const db = require('../db/connection');
+const table = require('console.table');
+
+// global variables for SQL
+const displayTable = `SELECT * FROM department`;
+
+// const function to get the table to display in rows
+const getTable = () => {
+    // return a promise in the database 
+    return db.promise().query(displayTable).then(([rows]) => {
+        return rows;
+    });
+};
+// shows table to the user via console logs
+const showTable = () => {
+    // run getTable() and console log the data to show the department table
+    getTable().then(data => {
+        console.table(data);
+    });
+};
+// get the department by the name
+const getDepartment = async() => {
+    const dName = `SELECT name FROM department
+    ORDER BY name`;
+    // try catch 
+    try {
+        // await with the db promise to get the department name from dName
+        const department = await db.promise().query(dName)
+        // returning the department and mapping the item to be the name
+        return department[0].map(item => item.name);
+        // catch error
+    } catch (err) {
+        console.log(err);
+    }
+};
+// ADD function to add another department name
+const addDepartment = async(name) => {
+    // sql variable to insert making a new department name
+    const sql = `INSERT INTO department (name) VALUES ('${name}')`;
+    // try catch
+    try {
+        // add const for the await db promise querying to sql
+        const add = await db.promise().query(sql);
+        // console log to display to the user their department was added to the table
+        console.log(`Added ${name} to the list of departments`);
+        // catch if there was error adding the department
+    } catch (err) {
+        console.log(err);
+    }
+};
+// DELETE function to delete a department
+const deleteDepartment = async(name) => {
+    // sql const to delete a department by the name
+    const sql = `DELETE FROM department WHERE name = '${name}'`;
+    // try catch
+    try {
+        // const deleted (since delete isnt allowed) for the db promise and query the sql 
+        const deleted = await db.promise().query(sql);
+        // console log to display to the user that the department was delete
+        console.log(`Deleted department ${name}`);
+    }
+    // catch if there was an error deleting the department
+    catch (err) {
+        console.log(err);
+    }
+};
+// const to find the department by the name
+const findDepartment = async(name) => {
+    const sql = `SELECT id FROM department WHERE name = '${name}'`;
+    // try catch
+    try {
+        // find const with await and querying the sql to find the department 
+        const find = await db.promise().query(sql)
+        return find[0][0].id;
+    // catch if there was an error finding the department
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+module.exports = {showTable, getDepartment, addDepartment, deleteDepartment, findDepartment};
---
+++ queries/query.js
@@ -0,0 +1,56 @@
+const db = require('../db/connection');
+const table = require('console.table');
+// const function to get the roles title
+const getRoles = (data) => { 
+    // empty array for the roles 
+    let roles = [];
+    // shows only role title
+    data.forEach(element => {
+        roles.push(element.Title);
+        
+    });
+};
+// const function to view roles
+const viewRoles = () => {
+    // select the role information based on the id, title, department, salary and role then joining department
+    const sql = `SELECT r.id ID, r.title Title, department.name Department, r.salary Salary
+                 FROM role r
+                 LEFT JOIN department ON r.department_id = department.id`;
+    // promise and query to create rows from the roles
+    db.promise().query(sql)
+        .then(([rows]) => {
+            getRoles(rows);
+            console.table(rows);
+        });
+};
+// const function to view employee information
+const viewEmployees = () => {
+    const sql = `SELECT e.id AS ID, CONCAT(e.first_name, ' ', e.last_name) AS Employee, role.title AS Title, department.name AS Department, role.salary AS Salary, CONCAT(m.first_name, ' ', m.last_name) AS Manager
+    FROM employee e
+    LEFT JOIN role ON e.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee m ON m.id = e.manager_id`;
+    // db query to generate rows and if there is an error it will log error otherwise will generate the table
+    db.query(sql, (err, rows) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.table(rows);
+        }
+    });
+};
+// const function to view the department name
+const viewDepartment = () => {
+    // just selecting the department name
+    const sql = `SELECT * FROM department`;
+    // db query to generate rows and if there is an error it will log error otherwise will generate the table
+    db.query(sql, (err, rows) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.table(rows);
+        }
+    });
+};
+
+module.exports = { viewRoles, viewEmployees, viewDepartment};
